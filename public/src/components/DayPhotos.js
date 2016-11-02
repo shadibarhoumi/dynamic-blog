3e752e080ca7cb19953c2dd69dce41ca241615f1
Shadi Barhoumi <shadibarhoumi@gmail.com>
2016-11-02 16:46:51 -0700

    css modules and webpack config to accomate video player css

--- public/src/components/DayPhotos.js
+++ public/src/components/DayPhotos.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import moment from 'moment'
 import Video from 'react-html5video'
 
+import 'react-html5video/dist/ReactHtml5Video.css'
+import styles from './DayPhotos.css'
+
 class DayPhotos extends Component {
   constructor(props) {
     super(props)
@@ -36,11 +39,15 @@ class DayPhotos extends Component {
         } else if (image.media === 'video') {
           return <li key={image._id}>
             <div>
-              <Video controls autoPlay loop>
+              <Video
+                controls autoPlay loop muted
+                className={styles.video}
+                width='500'
+              >
                 <source src={image.videoUrl} type='video/mp4' />
               </Video>
             </div>
-            <div>
+            <div className={styles.blue}>
               {moment(image.dateTaken).format('dddd, MMMM Do YYYY, h:mma')}
             </div>
             <div>
@@ -71,9 +78,9 @@ class DayPhotos extends Component {
     }
 
     return (
-      <div className="container">
-        <h1>Images for {moment(this.props.dateString, 'MM-DD-YYYY').format('dddd, MMMM Do YYYY')}</h1>
-        <ul className="list-group">
+      <div className='container'>
+        <h1 className={styles.blue}>Images for {moment(this.props.dateString, 'MM-DD-YYYY').format('dddd, MMMM Do YYYY')}</h1>
+        <ul className='list-group'>
           {this.renderImages(images)}
         </ul>
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,26 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel'
-    }]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: '/node_modules/',
+        loader: 'babel'
+      },
+      {
+        test: /ReactHtml5Video\.css$/,
+        loader: 'style!css'
+      },
+      {
+        test: /\.css$/,
+        exclude: /ReactHtml5Video\.css$/,
+        loader: 'style!css-loader?modules=1&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+      },
+      {
+        test: /\.(svg|woff|ttf|eot|png|jpg|gif)(\?.*)?$/i,
+        loader: 'url-loader?limit=10000'
+      }
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
